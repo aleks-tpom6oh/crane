8778a8834a07ae34d8833c240ed06e05728b4dd1
pvg-alex <alexey@plainvanillagames.com>
2014-12-11 14:50:56 +0300

    Launch password check

---
+++ src/main/java/com/tpom6oh/crane/LaunchPasswordChecker.java
@@ -0,0 +1,16 @@
+/**
+ * Created on 11.12.14
+ * @author alexey@plainvanillagames.com
+ */
+package com.tpom6oh.crane;
+
+import javax.inject.Inject;
+
+public class LaunchPasswordChecker
+{
+
+    @Inject
+    public LaunchPasswordChecker()
+    {
+    }
+}
